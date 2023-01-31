08eb29910d0d98f3a72a4b64c38c2e19f27080c6
Zeac <britezxavierluca@gmail.com>
2023-01-30 22:41:39 -0300

    Version final 1.1(Solicitud de direcciones)

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,33 @@
 import java.io.*;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Scanner;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /*Este programa tomara los datos de las dos primeras columnas de un excel(archivo xlsx),
-encriptara los datos de la segunda, guardara estos datos en un hashmap
-y volvera a guardar los datos ya encriptados dentro de otro archivo xlsx.
+encriptara los datos de la segunda, y volvera a guardar los datos ya encriptados dentro de otro archivo xlsx.
  La idea principal es utilizarlo en datos de tipo "Usario/contraseña" */
 public class Main {
     public static void main(String[] args) {
         try{
-            //Ingresar direccion del archivo xlsx que se desea encriptar en el parametro del constructor File.
+            //Mensaje inicial.
+            String solicitudPathInicial = "Porfavor ingrese la dirección del archivo a encriptar" +
+                    ",esto incluye el nombre del mismo y su extension(xlsx).";
+            System.out.println(solicitudPathInicial);
+            //Solicitud de la direccion del archivo a encriptar.
+            String pathInicial;
+            Scanner pathInicialTipeado = new Scanner(System.in);
+            pathInicial = pathInicialTipeado.nextLine();
 
-            File filePathIn = new File("C:/Users/Zeac/Desktop/TrabajoFinalJavaBasico/prueba.xlsx");
+            //Lectura del archivo a encriptar
+            File filePathIn = new File(pathInicial);
             FileInputStream fileIn = new FileInputStream(filePathIn);
 
+            //Se instancian los elementos que componen un archivo de excel y su iterador.
             XSSFWorkbook workbookIn= new XSSFWorkbook(fileIn);
             XSSFSheet sheetIn = workbookIn.getSheetAt(0);
             Iterator<Row> rowItr = sheetIn.iterator();
@@ -29,10 +39,11 @@ public class Main {
             String usuario = "";
             String password;
 
+            //While inicial, se utiliza para recorrer las columnas.
             while(rowItr.hasNext()){
                 Row row = rowItr.next();
                 Iterator<Cell> cellIterator = row.cellIterator();
-
+                //While final utilizado para iterar sobre las celdas del workbook.
                 while(cellIterator.hasNext()){
                     Cell cell = cellIterator.next();
 
@@ -42,31 +53,30 @@ public class Main {
                     //encriptadas dentro del HashMap map.
 
                     if(contador % 2 != 0){
-
                         usuario = cell.getStringCellValue();
-                        System.out.println("Impar: " + usuario);
-                    }else{
 
+                    }else{
                         password = cell.getStringCellValue();
-                        System.out.println("Par: " + password);
                         char [] encriptado = password.toCharArray();
                         for(int i = 0; i < encriptado.length; i++){
-                            encriptado[i] = (char)(encriptado [i] + (char)5);
+                        encriptado[i] = (char)(encriptado [i] + (char)5);
                         }
-
                         String passwordEncriptado = String.valueOf(encriptado);
-                        System.out.println("Password encriptada: " + passwordEncriptado);
-                        System.out.println("\n");
                         map.put(usuario,passwordEncriptado);
                     }
                 }
 
             }
-            System.out.println("Se termino de procesar el archivo \n\n");
+            System.out.println("-----Se termino de procesar el archivo----- \n\n");
 
-            //En el parametro del constructor File ingresar la direccion en donde se desea guardar el nuevo archivo
-            //Excel encriptado.
-            File filePathOut = new File("C:/Users/Zeac/Desktop/TrabajoFinalJavaBasico/Destino4.xlsx");
+            String pathDestino;
+            String solicitudPathDestino = "Porfavor ingrese la direccion en la cual desee guardar el archivo encriptado" +
+                    "y el nombre que desea que tenga el archivo encriptado junto con su extension(xlsx).";
+            System.out.println(solicitudPathDestino);
+            Scanner pathDestinoTipeado = new Scanner(System.in);
+            pathDestino = pathDestinoTipeado.nextLine();
+
+            File filePathOut = new File(pathDestino);
             XSSFWorkbook workbookOut = new XSSFWorkbook();
             XSSFSheet sheetOut = workbookOut.createSheet("Hoja 1");
 
@@ -86,7 +96,7 @@ public class Main {
             workbookOut.write(fileOut);
             fileOut.close();
 
-            System.out.println("Excel creado");
+            System.out.println("----Excel creado----");
 
         }catch(Exception e){
             e.printStackTrace();
